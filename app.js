68672a623304a733db628a974be165fa0fb95d2d
ahmetkfi <ahmetkcygt58@gmail.com>
2023-04-22 21:03:32 +0300

    clean blog task 2 icin commit

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 const express=require('express');
+const ejs=require('ejs');
+const path=require('path');
 const app=express();
 
-const blog = { id: 1, title: "Blog title", description: "Blog description" };
+//TEMPLATE ENGINE
+app.set('view engine','ejs');
+
+//MIDDLEWARES
+app.use(express.static('public'));
+
 
 app.get('/',(req,res)=>{
-    res.send(blog);
+    res.render('index');
+});
+app.get('/about',(req,res)=>{
+    res.render('about');
+});
+app.get('/add_post',(req,res)=>{
+    res.render('add_post');
 });
 const port=3000;
 app.listen(port,()=>{
